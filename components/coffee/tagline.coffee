9524f654a052c923b8cd40e2a37003957a357974
Yaman Tarakji <yamyoume@hotmail.com>
2017-08-15 12:01:07 +0300

    Added Browserify

--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,3 +1,5 @@
+$ = reqire 'jquery'
+
 do fill = (item = 'The most creative minds in art') ->
 	$('.tagline').append "#{item}"
 fill
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -1,7 +1,9 @@
-var fill;
+var $, fill;
+
+$ = reqire('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art');
+})('The most creative minds in art');
 
 fill;
